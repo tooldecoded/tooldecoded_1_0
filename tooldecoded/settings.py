c38381e4454aebc8eaa0c38804c3b65b80ff8698
John Doe <user@example.com>
2025-10-27 11:51:55 -0400

    Update templates and add static files

--- tooldecoded/settings.py
+++ tooldecoded/settings.py
@@ -140,6 +140,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
